cd5974f7e8b825685096a4367d845dd916ae4026
Vaibhavi Rohilla <62430272+vaibhavirohilla741@users.noreply.github.com>
2023-09-07 13:36:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import React , {useState,useEffect} from 'react'
+import './App.css'
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+
+function App() {
+  const [users,setUsers] = useState([])
+
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+console.log(users);
+  // note: the id field is mandatory
+  const items = users.map(user => (
+    user
+  ))
+
+  const handleOnSearch = (string, results) => {
+    // onSearch will have as the first callback parameter
+    // the string searched and for the second the results.
+    console.log(string, results)
+  }
+
+  const handleOnHover = (result) => {
+    // the item hovered
+    console.log(result)
+  }
+
+  const handleOnSelect = (item) => {
+    // the item selected
+    console.log(item)
+  }
+
+  const handleOnFocus = () => {
+    console.log('Focused')
+  }
+
+  const formatResult = (item) => {
+    return (
+      <>
+        
+        <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
+      </>
+    )
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{ width: 400 }}>
+          <ReactSearchAutocomplete
+            items={items}
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            autoFocus
+            formatResult={formatResult}
+          />
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
